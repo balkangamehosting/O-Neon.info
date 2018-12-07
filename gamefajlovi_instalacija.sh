8f2cca092295d3a663dec3924879c012a8420f8a
Ciker <itznaktuminajasiftw@gmail.com>
2018-12-07 14:05:06 +0100

    Create gamefajlovi_instalacija.sh
    Iskljuciti anonymous login u vsftpd posle instalacije i promeniti link za public mod od cs 1.6

---
+++ gamefajlovi_instalacija.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+echo "Skripta za automatsko instaliranje cs, mc, samp gamefajlova na centos 7 minimal by Root (Nikita Sibul)"
+yum update -y
+clear;
+echo "Update zavrsen!"
+
+yum install vsftpd
+clear;
+echo "FTP INSTALIRAN!"
+
+yum install glibc.i686 libstdc++.i686
+yum install wget
+yum install screen
+yum install unzip
+systemctl stop iptables
+systemctl disable iptables
+echo "Port 3144" >> /etc/ssh/sshd_config
+echo "chroot_local_user=YES" >> /etc/vsftpd/vsftpd.conf
+echo 'allow_writeable_chroot=YES' >> /etc/vsftpd/vsftpd.conf
+systemctl restart vsftpd
+systemctl enable vsftpd
+chmod -R 777 /home
+clear;
+echo "Instalirani fajlovi za igre!"
+
+mkdir /home/gamefiles
+cd /home/gamefiles/
+wget http://files.sa-mp.com/samp037svr_R2-1.tar.gz
+tar xf samp037svr_R2-1.tar.gz
+mv samp03 samp
+rm samp037svr_R2-1.tar.gz
+chmod -R 777 /home
+clear;
+echo "SAMP INSTALIRAN!"
+
+yum install java-1.8.0-openjdk
+yum install java-1.8.0-openjdk-devel
+mkdir /home/gamefiles/mc
+cd /home/gamefiles/mc
+wget https://cdn.getbukkit.org/spigot/spigot-1.8.8-R0.1-SNAPSHOT-latest.jar
+mv spigot-1.8.8-R0.1-SNAPSHOT-latest.jar spigot.jar
+echo "eula=true" > eula.txt
+chmod -R 777 /home
+clear;
+echo "MINECRAFT INSTALIRAN!"
+
+cd /home/gamefiles/
+wget http://cs.o-neon.host/dl/public.zip
+unzip public.zip
+rm public.zip
+chmod -R 777 /home
+clear;
+echo "CS 1.6 PUBLIC INSTALIRAN!"
+
+
+clear;
+echo "ZAVRSENA INSTALACIJA!"
